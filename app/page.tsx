889710f70d3149ca79e5897fd6ab187979b7c6dd
Rindra Hosea Mahefaniaina <mahefaniainarindra@gmail.com>
2025-10-26 16:47:07 +0300

    setup husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
