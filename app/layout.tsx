b30c343fe4cdd9596cf57d82384be33115a71db0
Rindra Hosea Mahefaniaina <mahefaniainarindra@gmail.com>
2025-10-26 18:40:39 +0300

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Techdoc Builder",
+  description: "Automatiser la création de documents techniques et schémas pour la gestion technique du bâtiment.",
 };
 
 const geistSans = Geist({
