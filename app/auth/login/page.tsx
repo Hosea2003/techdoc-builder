bc6f18c17d847685aa416eef84e592827e7a15b2
Rindra Hosea Mahefaniaina <mahefaniainarindra@gmail.com>
2025-10-26 16:25:32 +0300

    setup project: remove unecessary components migrate from middleware to proxy

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,9 @@
-import { LoginForm } from "@/components/login-form";
+import React from 'react'
 
-export default function Page() {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
-      </div>
-    </div>
-  );
+function LoginPage() {
+    return (
+        <div>LoginPage</div>
+    )
 }
+
+export default LoginPage
